4743166f2746226e3e858f21ac1021251095e939
Danny Cochran <daniel@cochrans.org>
2012-10-29 22:35:56 -0700

    styled location page

--- location.php
+++ location.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html> 
 <html>
 
@@ -13,17 +14,15 @@
 
 	<link rel="stylesheet" href="jquery.mobile-1.2.0.css" />
 	<link rel="stylesheet" href="white_theme.css" />
+	
 
 	<link rel="stylesheet" href="style.css" />
 	<link rel="apple-touch-icon" href="appicon.png" />
 	<link rel="apple-touch-startup-image" href="startup.png">
-
 	
 	<script src="jquery-1.8.2.min.js"></script>
 	<script src="jquery.mobile-1.2.0.js"></script>
 	
-	
-
 </head> 
 
 
@@ -33,17 +32,40 @@
 <div data-role="page" id="location">
 	<div data-role="header">
 		<a href="index.php" id="back" data-icon="custom" class = "top_bar_button"></a>
-		<h1><?php echo $place; ?></h1>
+		<h1>Memorial Church</h1>
 		<a href="#" id="fav" data-icon="custom" class = "top_bar_button"></a>
 
 
-	</div><!-- /header -->
+	</div>
 	
 	<div data-role="content">	
 	
+		<!-- photo container --> 
+		<div class="disgoProfilePhoto">
+		<img src="http://f.cl.ly/items/1g0H0B1y042l2K1m1d2Y/museosoumaya.png"/>
+			<p class="disgoProfilePhotoText">10 comments | 200 photos <br>0.6 miles away</p>
+		</div>
 		
+		<!-- add a comment popup --> 
+
+		<a href="#popupLogin" data-rel="popup" id="form" data-role="button" data-transition="pop" data-inline="true">Contribute</a>
+		<div id = "popupLogin" data-role="popup" data-theme="a" data-overlay-theme="c">
+		<form action="#" method="get">
+		<div data-role="fieldcontain" class="ui-hide-label"><textarea cols="40" rows="8" maxlength="140" name="textarea" placeholder="140 characters or less"></textarea></div>
+				<fieldset class="ui-grid-a">
+				<div class="ui-block-a"><button type="submit" data-theme="c">Cancel</button></div>
+				<div class="ui-block-b"><button type="submit" data-theme="b">Submit</button></div>	   
+			    </fieldset>
+		</form>
+		</div>
+		<!-- move to photoswipe gallery !-->
+
+
+		<! -- add a div container for new comments -->
+		<! -- add a div container for new comments -->
 
 	</div><!-- /content -->
+	
 	<?php
 		$local_state = "ui-btn-active ui-state-persist";
 		$global_state = "";
