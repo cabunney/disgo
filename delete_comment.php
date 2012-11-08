a640673ffe9a9ffe3c105200c4eb01a5c83ae6cd
Danny Cochran <daniel@cochrans.org>
2012-11-08 13:46:41 -0800

    delete comments from DOM & backend

--- delete_comment.php
+++ delete_comment.php
@@ -1,15 +1,11 @@
 <div class="commentarea">
 	<?php
 		include("config.php");
-		
-		$comment = $_POST["comment"];
 		$place = $_POST["place"];
 
-		$query = "DELETE FROM comments ('".$comment."', '".$place."')";
+		$query = "DELETE FROM comments where id='".$place."'";
 		$result1 = mysql_query($query);
-		
-		echo "<p>comment deleted</p>";
-	
+			
 	?>
 </div>
 
